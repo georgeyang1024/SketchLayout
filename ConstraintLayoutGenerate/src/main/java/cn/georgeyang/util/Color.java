7823f99735dbdf995fb4649285a6e7b73e81ba7d
georgeyang <georgeyang@gmail.com>
2018-01-20 14:38:39 +0800

    修复颜色值转换bug

--- ConstraintLayoutGenerate/src/main/java/cn/georgeyang/util/Color.java
+++ ConstraintLayoutGenerate/src/main/java/cn/georgeyang/util/Color.java
@@ -8,7 +8,7 @@ public class Color {
     public static final int BLACK = 0xff000000;
 
     public static final int argb(int a,int r,int g,int b) {
-        a = (r<<32) & 0xFF000000;//
+        a = (a<<24) & 0xFF000000;//
         r = (r << 16) & 0x00FF0000; //Shift red 16-bits and mask out other stuff
             g = (g << 8) & 0x0000FF00; //Shift Green 8-bits and mask out other stuff
             b = b & 0x000000FF; //Mask out anything not blue.
